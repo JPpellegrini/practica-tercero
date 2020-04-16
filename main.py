53f90f1b7f1d6fb6635d543ebbdcafba431f0d7f
Cristián Pérez <cristian@wenoa.com.ar>
2020-04-16 20:11:22 -0300

    Agrego formulario de login

--- main.py
+++ main.py
@@ -5,4 +5,15 @@ app = Flask(__name__)
 def do_login():
     pass
 
+@app.route('/login', methods=['GET'])
+def login_form():
+    return f"""<html>
+    <body>
+        <form action="/login" method="POST">
+            <input type="text" name="username" placeholder="Nombre de usuario" required>
+            <input type="submit" value="Ingresar"
+        </form>
+    </body>
+    </html>"""
+
 app.run()
