73081e2f8a57912d692c1b7683bd1643204381aa
Cristián Pérez <cristian@wenoa.com.ar>
2020-04-16 20:14:18 -0300

    Actualizo mensaje de bienvenida

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/login', methods=['POST'])
 def do_login():
-    pass
+    return f"Bienvenido {request.form['username']}. Tu contraseña es {request.form['password']}"
 
 app.run()
